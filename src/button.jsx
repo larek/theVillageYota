44d51a510815b5de705c45c14c73ea36f137b53e
Aleksandr Tikhonov <t9101029991@gmail.com>
2018-02-10 21:37:11 +0300

    functional component Button

--- src/button.jsx
+++ src/button.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
-export default class Button extends React.Component{
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return(
-      <div>
-        <img 
-          src={this.props.state[this.props.slug] ? './images/' + this.props.back : './images/' + this.props.front} 
-          data-slug={this.props.slug}
-          onMouseDown={this.props.toggle} 
-        />
-        <img src={'./images/' + this.props.back} style={{display:'none'}} alt=""/>
-      </div>
-    );
-  }
+export default ({state, back, front, slug, toggle}) => {
+  return(
+    <div>
+      <img 
+        src={state[slug] ? './images/' + back : './images/' + front} 
+        data-slug={slug}
+        onMouseDown={toggle} 
+      />
+      <img src={'./images/' + back} style={{display:'none'}} alt=""/>
+    </div>
+  );
 }
